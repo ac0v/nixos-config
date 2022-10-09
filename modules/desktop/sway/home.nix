6374d1361057bcacbec28c5ca5dfce0ec193fcad
ac0v <ac0v@sys-network.de>
2022-10-09 15:33:48 +0200

    enhanced sway config

--- modules/desktop/sway/home.nix
+++ modules/desktop/sway/home.nix
@@ -27,17 +27,18 @@ in
 
       startup = [                                       # Run commands on Sway startup
         {command = "${pkgs.autotiling}/bin/autotiling"; always = true;} # Tiling Script
-        ##{command = ''
-        ##  ${pkgs.swayidle}/bin/swayidle -w \
-        ##      before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
-        ##''; always = true;}                             # Lock on lid close (currently disabled because using laptop as temporary server)
-        #{command = ''
-        #  ${pkgs.swayidle}/bin/swayidle \
-        #    timeout 120 '${pkgs.swaylock-fancy}/bin/swaylock-fancy' \
-        #    timeout 240 'swaymsg "output * dpms off"' \
-        #    resume 'swaymsg "output * dpms on"' \
-        #    before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
-        #''; always = true;}                            # Auto lock\
+        { command = "systemctl --user restart waybar"; always = true; }
+        {command = ''
+          ${pkgs.swayidle}/bin/swayidle -w \
+              before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
+        ''; always = true;}                             # Lock on lid close (currently disabled because using laptop as temporary server)
+        {command = ''
+          ${pkgs.swayidle}/bin/swayidle \
+            timeout 120 '${pkgs.swaylock-fancy}/bin/swaylock-fancy' \
+            timeout 240 'swaymsg "output * dpms off"' \
+            resume 'swaymsg "output * dpms on"' \
+            before-sleep '${pkgs.swaylock-fancy}/bin/swaylock-fancy'
+        ''; always = true;}                            # Auto lock\
       ];
 
       bars = [];                                        # No bar because using Waybar
@@ -48,7 +49,7 @@ in
       };
 
       gaps = {                                          # Gaps for containters
-        inner = 5;
+        smartBorders = "on";
         outer = 5;
       };
 
@@ -90,7 +91,7 @@ in
         "${modifier}+l" = "exec ${pkgs.swaylock-fancy}/bin/swaylock-fancy"; # Lock Screen
 
         "${modifier}+r" = "reload";                     # Reload environment
-        "${modifier}+q" = "kill";                       # Kill container
+        "${modifier}+Shift+q" = "kill";                 # Kill container
         "${modifier}+f" = "fullscreen toggle";          # Fullscreen
         "${modifier}+h" = "floating toggle";            # Floating
 
@@ -104,25 +105,28 @@ in
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Down" = "move down";
 
-        #"Alt+Left" = "workspace prev";                  # Navigate to previous or next workspace if it exists
-        #"Alt+Right" = "workspace next";
-        "Alt+Left" = "workspace prev_on_output";         # Navigate to previous or next workspace on output if it exists
-        "Alt+Right" = "workspace next_on_output";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
 
-        "Alt+1" = "workspace number 1";                 # Open workspace x
-        "Alt+2" = "workspace number 2";
-        "Alt+3" = "workspace number 3";
-        #"Alt+4" = "workspace number 4";
-        #"Alt+5" = "workspace number 5";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
 
-        "Alt+Shift+Left" = "move container to workspace prev, workspace prev";    # Move container to next available workspace and focus
-        "Alt+Shift+Right" = "move container to workspace next, workspace next";
-
-        "Alt+Shift+1" = "move container to workspace number 1";     # Move container to specific workspace
-        "Alt+Shift+2" = "move container to workspace number 2";
-        "Alt+Shift+3" = "move container to workspace number 3";
-        "Alt+Shift+4" = "move container to workspace number 4";
-        "Alt+Shift+5" = "move container to workspace number 5";
 
         "Control+Up" = "resize shrink height 20px";     # Resize container
         "Control+Down" = "resize grow height 20px";
@@ -145,6 +149,7 @@ in
     };
     extraConfig = ''
       set $opacity 0.8
+      default_border pixel 1
       for_window [class=".*"] opacity 0.95
       for_window [app_id=".*"] opacity 0.95
       for_window [app_id="pcmanfm"] opacity 0.95, floating enable
